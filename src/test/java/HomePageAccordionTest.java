d0444fe002ff0029ae37f44395a00930e8361566
dmitriy <dmitriy@mail.com>
2023-01-28 19:41:58 +0300

    Добавление браузера Firefox

--- src/test/java/HomePageAccordionTest.java
+++ src/test/java/HomePageAccordionTest.java
@@ -1,29 +1,54 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import page.HomePage;
-
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
+@RunWith(Parameterized.class)
 public class HomePageAccordionTest {
+
     private WebDriver driver;
+    private final String browserName;
+
+    public HomePageAccordionTest(String browserName) {
+        this.browserName = browserName;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Chrome"},
+                {"Firefox"},
+        };
+    }
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://qa-scooter.praktikum-services.ru/");
+        switch (browserName) {
+            case "Chrome":
+                driver = new ChromeDriver();
+                driver.get("https://qa-scooter.praktikum-services.ru/");
+                break;
+            case "Firefox":
+                driver = new FirefoxDriver();
+                driver.get("https://qa-scooter.praktikum-services.ru/");
+                break;
+        }
     }
+
     @Test
     public void checkAccordionTextAfterClickExists() {
         HomePage homePage = new HomePage(driver);
         homePage.waitForLoadAccordion();
         homePage.clickAccordionElements();
 
-        assertThat(homePage.getTextsFromAccordion(), everyItem(not(containsString(""))));
+        assertThat("Элемент выпадающего списка пустой", homePage.getTextsFromAccordion(), everyItem(not(containsString(""))));
     }
 
     @After
