f757e335fdb2ccd5b79dd803f9a9ffefe69e492c
dmitriy <dmitriy@mail.com>
2023-01-31 21:02:51 +0300

    Добавлен класс с константами

---
+++ src/main/java/constant/Env.java
@@ -0,0 +1,6 @@
+package constant;
+
+public class Env {
+    public static final String BROWSER_NAME = "Chrome"; //  || Firefox
+    public static final String URL = "https://qa-scooter.praktikum-services.ru";
+}
