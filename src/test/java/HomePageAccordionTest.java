1516f6063eb0b3b08da41a64e85b063ebd651194
dmitriy <dmitriy@mail.com>
2023-01-31 21:01:57 +0300

    Обновление теста с аккордеоном

--- src/test/java/HomePageAccordionTest.java
+++ src/test/java/HomePageAccordionTest.java
@@ -1,3 +1,4 @@
+import constant.Env;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,37 +8,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import page.HomePage;
-import static org.hamcrest.CoreMatchers.*;
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(Parameterized.class)
 public class HomePageAccordionTest {
 
     private WebDriver driver;
-    private final String browserName;
+    private final int index;
+    private final String text;
 
-    public HomePageAccordionTest(String browserName) {
-        this.browserName = browserName;
+    public HomePageAccordionTest(int index, String text) {
+        this.index = index;
+        this.text = text;
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters()
     public static Object[][] getData() {
         return new Object[][]{
-                {"Chrome"},
-                {"Firefox"},
+                {1, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {2, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {3, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {4, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {5, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {6, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {7, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {8, "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
         };
     }
 
     @Before
     public void setUp() {
-        switch (browserName) {
-            case "Chrome":
-                driver = new ChromeDriver();
-                driver.get("https://qa-scooter.praktikum-services.ru/");
-                break;
+        switch (Env.BROWSER_NAME) {
             case "Firefox":
                 driver = new FirefoxDriver();
-                driver.get("https://qa-scooter.praktikum-services.ru/");
+                driver.get(Env.URL);
+                break;
+            default:
+                driver = new ChromeDriver();
+                driver.get(Env.URL);
                 break;
         }
     }
@@ -46,9 +56,9 @@ public class HomePageAccordionTest {
     public void checkAccordionTextAfterClickExists() {
         HomePage homePage = new HomePage(driver);
         homePage.waitForLoadAccordion();
-        homePage.clickAccordionElements();
+        homePage.clickAccordionElement(index - 1);
 
-        assertThat("Элемент выпадающего списка пустой", homePage.getTextsFromAccordion(), everyItem(not(containsString(""))));
+        assertThat("Элемент выпадающего списка не соответствует тексту", homePage.getTextFromAccordion(index - 1), containsString(text));
     }
 
     @After
